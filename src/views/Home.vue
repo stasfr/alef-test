d5599a6779b43c9ab45a0dab78672b5366423b4a
stas_fr <stas240600@yandex.ru>
2024-09-11 12:56:10 +0300

    zeroing project + basic structure

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="_container">Home page</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "",
+  components: {},
+  props: {},
+  setup(props) {},
+});
+</script>
+
+<style scoped></style>
